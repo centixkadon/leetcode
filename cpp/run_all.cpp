e25d33eb045254b99881a753d22f8c5af83fc2ea
centixkadon <centixkadon@gmail.com>
2018-05-08 20:39:47 +0800

    Update run_all

--- cpp/run_all.cpp
+++ cpp/run_all.cpp
@@ -5,7 +5,14 @@ int main(int argc, char ** argv) {
   string s = argv[0];
   s = s.substr(0, s.find_last_of('/') + 1) + "p";
 
-  for (size_t i = 0; i != (size_t)-1; ++i) {
+  size_t index_begin = 0, index_end = -1;
+  if (argc > 1) stringstream(argv[1]) >> index_begin;
+  if (argc > 2) stringstream(argv[2]) >> index_end;
+
+  cout << "leetcode run all" << endl;
+  cout << endl;
+
+  for (size_t i = index_begin; i != index_end; ++i) {
     string fs = s + (static_cast<stringstream &>(stringstream() << setw(4) << setfill('0') << i).str());
     if (ifstream(fs)) {
       if (system(fs.c_str()) != 0) break;
