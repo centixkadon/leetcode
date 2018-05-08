0193136322a4e08d3976a2495fdb4266f0bb538e
centixkadon <centixkadon@gmail.com>
2018-05-08 16:53:06 +0800

    Create run_all Run all leetcode cpp solution

--- cpp/run_all.cpp
+++ cpp/run_all.cpp
@@ -3,12 +3,12 @@
 
 int main(int argc, char ** argv) {
   string s = argv[0];
-  s = s.substr(0, s.size() - 4);
+  s = s.substr(0, s.find_last_of('/') + 1) + "p";
 
   for (size_t i = 0; i != (size_t)-1; ++i) {
     string fs = s + (static_cast<stringstream &>(stringstream() << setw(4) << setfill('0') << i).str());
     if (ifstream(fs)) {
-      int i = system(fs.c_str());
+      if (system(fs.c_str()) != 0) break;
       cout << endl;
     } else
       break;
