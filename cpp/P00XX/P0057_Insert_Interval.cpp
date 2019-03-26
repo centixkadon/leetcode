a71717b693e805c968007ad55582e75389f1b987
centixkadon <centixkadon@gmail.com>
2019-03-26 21:18:45 +0800

    Add P0057_Insert_Interval

---
+++ cpp/P00XX/P0057_Insert_Interval.cpp
@@ -0,0 +1,68 @@
+
+#include "leetcode.hpp"
+
+class Solution {
+public:
+  vector<Interval> insert(vector<Interval> & intervals, Interval newInterval) {
+    if (intervals.empty()) return { newInterval };
+
+    size_t l = 0, r = intervals.size();
+    while (l < r) {
+      size_t m = (l + r) >> 1;
+      if (intervals[m].end < newInterval.start) {
+        l = m + 1;
+      } else {
+        r = m;
+      }
+    }
+    if (l == intervals.size() || newInterval.end < intervals[l].start) {
+      intervals.insert(intervals.begin() + l, newInterval);
+    } else {
+      size_t s = l, r = intervals.size();
+      while (l < r) {
+        size_t m = (l + r) >> 1;
+        if (intervals[m].start <= newInterval.end) {
+          l = m + 1;
+        } else {
+          r = m;
+        }
+      }
+      intervals[s].start = min(intervals[s].start, newInterval.start);
+      intervals[s].end = max(intervals[l - 1].end, newInterval.end);
+      if (s + 1 != l) {
+        for (size_t i = l; i < intervals.size(); ++i) {
+          ++s;
+          intervals[s] = intervals[i];
+        }
+        intervals.resize(s + 1);
+      }
+    }
+    return move(intervals);
+  }
+};
+
+int main() {
+  {
+    auto check = solve(&Solution::insert);
+    check({ { 2, 5 } }, {}, { 2, 5 });
+    check({ { 1, 5 }, { 6, 9 } }, { { 1, 3 }, { 6, 9 } }, { 2, 5 });
+    check({ { 1, 2 }, { 3, 10 }, { 12, 16 } }, { { 1, 2 }, { 3, 5 }, { 6, 7 }, { 8, 10 }, { 12, 16 } }, { 4, 8 });
+    check({ { 1, 2 }, { 3, 5 } }, { { 1, 2 } }, { 3, 5 });
+    check({ { 2, 5 } }, { { 2, 3 } }, { 3, 5 });
+    check({ { 2, 5 } }, { { 2, 4 } }, { 3, 5 });
+    check({ { 2, 5 } }, { { 2, 5 } }, { 3, 5 });
+    check({ { 2, 6 } }, { { 2, 6 } }, { 3, 5 });
+    check({ { 3, 6 } }, { { 3, 6 } }, { 3, 5 });
+    check({ { 3, 6 } }, { { 4, 6 } }, { 3, 5 });
+    check({ { 3, 6 } }, { { 5, 6 } }, { 3, 5 });
+    check({ { 3, 5 }, { 6, 7 } }, { { 6, 7 } }, { 3, 5 });
+    check({ { 1, 1 }, { 2, 4 } }, { { 1, 1 } }, { 2, 4 });
+    check({ { 2, 4 } }, { { 2, 2 } }, { 2, 4 });
+    check({ { 2, 4 } }, { { 3, 3 } }, { 2, 4 });
+    check({ { 2, 4 } }, { { 4, 4 } }, { 2, 4 });
+    check({ { 2, 4 }, { 5, 5 } }, { { 5, 5 } }, { 2, 4 });
+  }
+  return 0;
+}
+
+Settings;
